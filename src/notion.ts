91ca56d5073778019f15b672edd87a2ef599d749
박성훈 / Koder <koder0205@gmail.com>
2025-01-03 19:14:50 +0900

    refactor : switch-case is suck

--- src/notion.ts
+++ src/notion.ts
@@ -3,33 +3,23 @@ const { Client } = require("@notionhq/client");
 const notion = new Client({ auth: process.env.NOTION });
 
 function parseProp(prop: any): any {
-  if (prop["type"] == "email") return prop["email"];
   try {
-    switch (prop["type"]) {
-      case "title":
-        return prop["title"][0]["plain_text"];
-      case "email":
-        return prop["email"];
-      case "number":
-        return prop["number"];
-      case "rich_text":
-        return prop["rich_text"][0]["plain_text"];
-      case "phone_number":
-        return prop["phone_number"];
-      case "checkbox":
-        return prop["checkbox"];
-      default:
-        return null;
-    }
+    if (prop["type"] == "title") return prop["title"][0]["plain_text"];
+    if (prop["type"] == "email") return prop["email"];
+    if (prop["type"] == "number") return prop["number"];
+    if (prop["type"] == "rich_text") return prop["rich_text"][0]["plain_text"];
+    if (prop["type"] == "phone_number") return prop["phone_number"];
+    if (prop["type"] == "checkbox") return prop["checkbox"];
   } catch (e) {
     return null;
   }
+  return null;
 }
 
 async function Query(type: "Member" | "Achievements"): Promise<any> {
   let databaseId;
   if (type == "Member") databaseId = process.env.MEMBERDB;
-  else databaseId = process.env.ACHIEVEDB;
+  if (type == "Achievements") databaseId = process.env.ACHIEVEDB;
 
   return await notion.databases.query({
     database_id: databaseId,
